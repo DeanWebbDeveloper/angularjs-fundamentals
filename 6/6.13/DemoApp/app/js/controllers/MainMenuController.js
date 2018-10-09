010bc47a3a3321b56ac3865f4841011ed87d0bb5
Dean Webb Developer <dean@deanwebbdeveloper.com>
2018-10-09 15:26:24 +0100

    6.13-6.14
    Completed 2 modules, to do suggested exercises (6.15).

---
+++ 6/6.13/DemoApp/app/js/controllers/MainMenuController.js
@@ -0,0 +1,10 @@
+'use strict'
+
+eventsApp.controller('MainMenuController',
+  function MainMenuController($scope, $location) {
+    $scope.createEvent = function() {
+      $location.replace();
+      $location.url('/newEvent');
+    };
+  }
+);
